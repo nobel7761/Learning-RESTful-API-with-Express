c988218ea3e5ce55a888d0f4c7efd2a221c1d277
nobel7761 <habiburnobel@gmail.com>
2021-12-02 18:41:02 +0600

    Handling POST Request using Postman

--- My-Express/app.js
+++ My-Express/app.js
@@ -2,23 +2,28 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-    response.send('Hello Express World!');
+    response.send("Hello from express world!");
 })
 
 app.get('/api/students', (request, response) => {
     fs.readFile('./db.json', 'utf-8', (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            const students = JSON.parse(data).students;
-            response.send(students);
-        }
+        response.send(data);
     })
 })
 
-
+app.post('/api/students', (request, response) => {
+    const student = request.body;
+    fs.readFile('./db.json', 'utf-8', (err, data) => {
+        const students = JSON.parse(data);
+        students.students.push(student);
+        fs.writeFile('./db.json', JSON.stringify(students), (err) => {
+            response.send(student);
+        })
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
