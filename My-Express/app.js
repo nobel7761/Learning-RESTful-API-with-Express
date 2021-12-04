73878d538f2f3ceca64e62dce49892d70f50d344
nobel7761 <habiburnobel@gmail.com>
2021-12-05 04:00:56 +0600

    urlencoded middleware used

--- My-Express/app.js
+++ My-Express/app.js
@@ -4,6 +4,8 @@ const studentRouter = require('./routers/studentRouter');
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use((req, res, next) => {
     console.log("I am middleware 1!");
     next();
