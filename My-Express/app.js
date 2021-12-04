f2e042d2db34eb86b7b94516edbf744f64208020
nobel7761 <habiburnobel@gmail.com>
2021-12-05 03:58:43 +0600

    writing custome middleware

--- My-Express/app.js
+++ My-Express/app.js
@@ -3,10 +3,22 @@ const app = express();
 const studentRouter = require('./routers/studentRouter');
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("I am middleware 1!");
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("I am middleware 1!");
+    next();
+})
+
+app.use(express.urlencoded({ extended: true }))
 app.use('/api/students', studentRouter);
 
-
 app.get('/', (request, response) => {
+    console.log('I am GET Request Middleware!');
     response.send("Hello from express world!");
 })
 
