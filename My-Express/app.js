ef0e274bd759bbad2b665bd77e32ab17db4f160a
nobel7761 <habiburnobel@gmail.com>
2021-12-05 04:04:17 +0600

    applied morgan middleware

--- My-Express/app.js
+++ My-Express/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const studentRouter = require('./routers/studentRouter');
+const morgan = require('morgan');
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
+
+app.use(morgan('combined'))
 
 app.use((req, res, next) => {
     console.log("I am middleware 1!");
